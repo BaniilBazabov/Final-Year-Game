162e04ad74c8129f04b36138b9c111ba96ff0a1a
baniilbazabov <daniilkazakov8701@gmail.com>
2023-12-15 17:24:24 +0000

    randomised skills added, randomiser broken

--- Scripts/LevelUpScreen.cs
+++ Scripts/LevelUpScreen.cs
@@ -28,22 +28,6 @@ public partial class LevelUpScreen : Control
 	{
 	}
 
-	public void ShowMenu()
-	{
-		Show();
-	}
-
-	public void HideMenu()
-	{
-		Hide();
-	}
-
-	private void _onUpgradeButtonPressed()
-	{
-		HideMenu();
-
-	}
-
 	public class Upgrade
 	{
 		public string IconPath { get; set; }
@@ -60,17 +44,76 @@ public partial class LevelUpScreen : Control
 			Tier = tier;
 			MaxTier = maxTier;
 		}
+	}
 
-		private List<Upgrade> allUpgrades = new List<Upgrade>
+	private List<Upgrade> allUpgrades = new List<Upgrade>
+	{
+		new Upgrade("res://Art/Skills/BonkHammer.png", "Bonking Hammer ", "Increases your damage from all sources by 10%", 1, 5),
+		new Upgrade("res://Art/Skills/hairPreservedSprinkles.png", "Hair Preserved ", "Increases your HP by 10%", 1, 5),
+		new Upgrade("res://icon.svg", "Placeholder 1", "ToBeReplaced 1", 1, 5),
+		new Upgrade("res://icon.svg", "Placeholder 2", "ToBeReplaced 2", 1, 5),
+		new Upgrade("res://icon.svg", "Placeholder 3", "ToBeReplaced 3", 1, 5),
+		new Upgrade("res://icon.svg", "Placeholder 4", "ToBeReplaced 4", 1, 5),
+	};
+
+	private List<Upgrade> availableUpgrades = new List<Upgrade>();
+
+	public void ShowMenu()
+	{
+		Randomize(allUpgrades);
+		foreach (Upgrade upgrade in allUpgrades)
 		{
-			new Upgrade("res://Art/Skills/BonkHammer.png", "Bonking Hammer ", "Increases your damage from all sources by 10%", 1, 5),
-			new Upgrade("res://Art/Skills/hairPreservedSprinkles.png", "Hair Preserved ", "Increases your HP by 10%", 1, 5),
-			new Upgrade("res://icon.svg", "Placeholder 1", "ToBeReplaced 1", 1, 5),
-			new Upgrade("res://icon.svg", "Placeholder 2", "ToBeReplaced 2", 1, 5),
-			new Upgrade("res://icon.svg", "Placeholder 3", "ToBeReplaced 3", 1, 5),
-			new Upgrade("res://icon.svg", "Placeholder 4", "ToBeReplaced 4", 1, 5),
-		};
+			GD.Print(upgrade.Name);
+		}
+		
+		// availableUpgrades = allUpgrades.GetRange(0, 3);
+		// // Assign upgrades to buttons based on tier
+		// foreach (Upgrade upgrade in allUpgrades)
+		// {
+		// 	// Check if the upgrade's tier is less than or equal to its max tier
+		// 	if (upgrade.Tier <= upgrade.MaxTier)
+		// 	{
+		// 		// Assign the upgrade to the list of available upgrades
+		// 		availableUpgrades.Add(upgrade);
 
-		private List<Upgrade> availableUpgrades = new List<Upgrade>();
+		// 		// Increment the tier for the next availability
+		// 		upgrade.Tier++;
+		// 	}
+		// }
+
+		// // Assign upgrades to buttons
+		// for (int i = 0; i < availableUpgrades.Count; i++)
+		// {
+		// 	Button button = GetNode<Button>($"UpgradeButton{i + 1}");
+		// 	button.Text = availableUpgrades[i].Name;
+		// 	// Set the icon and explanation text as needed
+		// 	// button.Icon = LoadIcon(availableUpgrades[i].IconPath);
+		// 	// button.ExplanationText = availableUpgrades[i].Description;
+		// }
+
+		Show();
+	}
+
+	public void HideMenu()
+	{
+		Hide();
+	}
+
+	private void _onUpgradeButtonPressed()
+	{
+		HideMenu();
+	}
+
+	public static List<T> Randomize<T>(List<T> list)
+	{
+		List<T> randomizedList = new List<T>();
+		Random rnd = new Random();
+		while (list.Count > 0)
+		{
+			int index = rnd.Next(0, list.Count); //pick a random item from the master list
+			randomizedList.Add(list[index]); //place it at the end of the randomized list
+			list.RemoveAt(index);
+		}
+		return randomizedList;
 	}
 }
