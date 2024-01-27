662cbbfb65fca32f443c576656663949197bfb06
BaniilBazabov <daniilkazakov8701@gmail.com>
2024-01-27 18:51:08 +0000

    Mob attack improved

--- Scripts/Mob.cs
+++ Scripts/Mob.cs
@@ -12,6 +12,7 @@ public partial class Mob : RigidBody2D
 	private Timer attackCooldown;
 	[Export] public float maxHealth = 100f;
 	public float health;
+	Area2D mobHitbox;
 	ProgressBar bar;
 
 	// Called when the node enters the scene tree for the first time.
@@ -23,6 +24,7 @@ public partial class Mob : RigidBody2D
 
 		player = GetTree().Root.GetNode<Player>("Game/Player");
 
+		mobHitbox = GetNode<Area2D>("MobHitbox");
 		attackCooldown = GetNode<Timer>("AttackCooldown");
 		health = maxHealth;
 		bar = GetNode<ProgressBar>("MobHealthBar");
@@ -34,25 +36,42 @@ public partial class Mob : RigidBody2D
 	{
 		UpdateMobHealthBar();
 
-		if (player != null)
-		{
-			Vector2 direction = player.GlobalPosition - GlobalPosition;
-			float distanceToPlayer = direction.Length();
+		Vector2 direction = player.GlobalPosition - GlobalPosition;
+		
+		// If not attacking, move the mob towards the player
+		direction = direction.Normalized();
+		Vector2 velocity = direction * (float)(speed * delta);
+		GlobalPosition += velocity;
 
-			if (distanceToPlayer <= 5)
+		foreach (Node2D player in mobHitbox.GetOverlappingAreas())
+		{
+			if (player is Player)
 			{
 				Attack();
-			} 
-			else 
-			{
-				// If not attacking, move the mob towards the player
-				direction = direction.Normalized();
-				Vector2 velocity = direction * (float)(speed * delta);
-				GlobalPosition += velocity;
 			}
 		}
 
 	}
+
+	// public void Attack()
+	// {
+	// 	if (AttackCooldown.IsStopped())
+	// 	{
+	// 		AnimatedSprite2D attackAnimation = GetNode<AnimatedSprite2D>("AttackAnimation");
+	// 		Area2D attackZone = GetNode<Area2D>("AttackZone");
+	// 		foreach (Node2D mob in attackZone.GetOverlappingBodies())
+	// 		{
+	// 			if (mob is Mob)
+	// 			{
+	// 				Mob mobInstance = (Mob)mob;
+	// 				mobInstance.Damage(damage);
+	// 			}
+	// 		}
+
+	// 		attackAnimation.Play("oneshot");
+	// 		AttackCooldown.Start();
+	// 	}
+	// }
 
 	public void Attack()
 	{
