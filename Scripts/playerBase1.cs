59e11251d4838a6f38330d2b42faa414e36042a2
BaniilBazabov <daniilkazakov8701@gmail.com>
2024-02-27 12:04:39 +0000

    teleportation to level added

--- Scripts/playerBase1.cs
+++ Scripts/playerBase1.cs
@@ -3,10 +3,15 @@ using System;
 
 public partial class playerBase1 : Node2D
 {
+	Area2D teleportArea;
 	PauseMenu pauseMenu;
+	private Sprite2D e;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		e = GetNode<Sprite2D>("E");
+		e.Visible = false;
+		teleportArea = GetNode<Area2D>("TeleportArea");
 		pauseMenu = GetNode<PauseMenu>("PauseMenu");
 		pauseMenu.Hide();
 	}
@@ -17,6 +22,26 @@ public partial class playerBase1 : Node2D
 		if(Input.IsActionJustPressed("pause_menu"))
 		{
 			_PauseGame();
+		}
+
+		bool isInTeleportArea = false;
+
+		foreach (Node2D body in teleportArea.GetOverlappingBodies())
+		{
+			if (body is Player)
+			{
+				isInTeleportArea = true;
+				break;
+			}
+		}
+
+		// Set the visibility of the "E" sprite based on player's position
+		e.Visible = isInTeleportArea;
+
+		// Teleportation logic
+		if (isInTeleportArea && Input.IsActionJustPressed("interact"))
+		{
+			GetTree().ChangeSceneToFile("res://Scripts/map.tscn");
 		}
 	}
 	private void _PauseGame()
@@ -34,6 +59,4 @@ public partial class playerBase1 : Node2D
 	{
 		GetTree().Quit();
 	}
-
-
 }
